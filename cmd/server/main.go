f2ee8ea8d21fe557472c546d0641723388a27286
anmol420 <anmolanand2k15@gmail.com>
2026-05-31 18:17:29 +0530

    fix: removed smoke test

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -16,12 +16,9 @@ func main() {
 		Level: slog.LevelInfo,
 	}))
 	slog.SetDefault(logger)
-
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
-
 	DATABASE_ADDR := env.StringGetEnv("DATABASE_ADDR", slog.Default())
-
 	pool, err := db.DbConnect(ctx, DATABASE_ADDR)
 	if err != nil {
 		slog.Error("Failed to connect to database", "error", err)
@@ -29,26 +26,6 @@ func main() {
 	}
 	defer pool.Close()
 	slog.Info("DB connection successful")
-
-	// smoke test
-	repo := db.NewRepository(pool)
-	acc, err := repo.CreateAccount(ctx, db.CreateAccountParams{
-		OwnerName: "Anmol",
-		Balance: 2000,
-	})
-	if err != nil {
-		slog.Error("create account failed", "error", err)
-		os.Exit(1)
-	}
-	slog.Info("account created", "id", acc.ID, "owner", acc.OwnerName, "balance", acc.Balance)
-	fetched, err := repo.GetAccount(ctx, acc.ID)
-	if err != nil {
-		slog.Error("get account failed", "error", err)
-		os.Exit(1)
-	}
-	slog.Info("account fetched", "id", fetched.ID, "balance", fetched.Balance)
-	// test done
-	
 	<-ctx.Done()
 	slog.Info("Server shutting down")
 }
