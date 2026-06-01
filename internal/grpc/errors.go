1a2a02422cbc042ebc5a182a1e3b134eb3ba57ae
anmol420 <anmolanand2k15@gmail.com>
2026-06-02 01:26:31 +0530

    added go layer for proto file

--- internal/grpc/errors.go
+++ internal/grpc/errors.go
@@ -1,1 +1,24 @@
 package grpc
+
+import (
+	"errors"
+
+	"github.com/anmol420/p2p-payment-ledger/internal/service"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func toGRPCError(err error) error {
+	switch {
+	case errors.Is(err, service.ErrInsufficientFunds):
+		return status.Errorf(codes.FailedPrecondition, "Insufficient funds: %v", err)
+	case errors.Is(err, service.ErrAccountNotFound):
+		return status.Errorf(codes.NotFound, "Account not found: %v", err)
+	case errors.Is(err, service.ErrSameAccount):
+		return status.Errorf(codes.InvalidArgument, "Cannot transfer funds to same account: %v", err)
+	case errors.Is(err, service.ErrInvalidAmount):
+		return status.Errorf(codes.InvalidArgument, "Invalid amount: %v", err)
+	default:
+		return status.Errorf(codes.Internal, "Internal error: %v", err)
+	}
+}
--- internal/grpc/server.go
+++ internal/grpc/server.go
@@ -1,1 +1,59 @@
 package grpc
+
+import (
+	"context"
+	"log/slog"
+
+	pb "github.com/anmol420/p2p-payment-ledger/gen/ledger/v1"
+	"github.com/anmol420/p2p-payment-ledger/internal/db"
+	"github.com/anmol420/p2p-payment-ledger/internal/service"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+type Server struct {
+	pb.UnimplementedLedgerServiceServer
+	transferSvc *service.TransferService
+	repo        db.Repository
+	logger      *slog.Logger
+}
+
+func NewServer(
+	transferSvc *service.TransferService,
+	repo db.Repository,
+	logger *slog.Logger,
+) *Server {
+	return &Server{
+		transferSvc: transferSvc,
+		repo:        repo,
+		logger:      logger,
+	}
+}
+
+func (s *Server) CreateAccount(
+	ctx context.Context,
+	req *pb.CreateAccountRequest,
+) (*pb.CreateAccountResponse, error) {
+	return nil, status.Error(codes.Unimplemented, "not implemented")
+}
+
+func (s *Server) GetBalance(
+	ctx context.Context,
+	req *pb.GetBalanceRequest,
+) (*pb.GetBalanceResponse, error) {
+	return nil, status.Error(codes.Unimplemented, "not implemented")
+}
+
+func (s *Server) Transfer(
+	ctx context.Context,
+	req *pb.TransferRequest,
+) (*pb.TransferResponse, error) {
+	return nil, status.Error(codes.Unimplemented, "not implemented")
+}
+
+func (s *Server) ListTransactions(
+	ctx context.Context,
+	req *pb.ListTransactionsRequest,
+) (*pb.ListTransactionsResponse, error) {
+	return nil, status.Error(codes.Unimplemented, "not implemented")
+}
